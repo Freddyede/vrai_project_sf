5060c4f3edc30a3df9719d154d297b9f87ae2c98
Freddyede <f.patouillard@it-students.fr>
2022-07-19 15:50:08 +0200

    add database to project

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,17 @@
 
 namespace App\Controller;
 
+use App\Services\HomeServices;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
-{
+class HomeController extends AbstractController {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(HomeServices $HS): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'controller_name' => $HS->getAllDatas(),
         ]);
     }
 }
---
+++ src/Services/HomeServices.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Services;
+
+class HomeServices {
+    function getAllDatas(): string {
+        return "HomeController";
+    }
+}
